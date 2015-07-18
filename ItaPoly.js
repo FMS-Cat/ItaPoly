827b38cf885a138bc744d9d2ee399c51457fec7a
FMS_Cat <ev40574@meiji.ac.jp>
2015-07-19 01:39:01 +0900

    いろいろやった

--- ItaPoly.js
+++ ItaPoly.js
@@ -23,37 +23,11 @@ ItaPoly = function( _width, _height ){
 
   it.vert = 'attribute vec2 p;void main(){gl_Position=vec4(p,0.,1.);}';
 
-  it.createProgram( '#ifdef GL_ES\nprecision mediump float;\n#endif\nuniform float time;uniform vec2 resolution;uniform sampler2D texture0;void main(){gl_FragColor=texture2D(texture0, vec2( gl_FragCoord.x/resolution.x, 1.-gl_FragCoord.y/resolution.y ) );}' );
-
-  it.vbo = ( function(){
-		var vbo = gl.createBuffer();
-
-		gl.bindBuffer( gl.ARRAY_BUFFER, vbo );
-		gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [-1,1,-1,-1,1,1,1,-1] ), gl.STATIC_DRAW );
-		gl.bindBuffer( gl.ARRAY_BUFFER, null );
-
-		return vbo;
-	} () );
-	gl.bindBuffer( gl.ARRAY_BUFFER, it.vbo );
-
-  it.location = {};
-  it.location.p = gl.getAttribLocation( it.program, 'p' );
-	gl.enableVertexAttribArray( it.location.p );
-	gl.vertexAttribPointer( it.location.p, 2, gl.FLOAT, false, 0, 0 );
+  it.location = [];
 
   it.textures = []
   it.textureNumbers = []
   it.textureCount = 0;
-  for( i=0; i<4; i++ ){
-    gl.activeTexture( 33984 + i ); // gl.TEXTURE0 = 33984
-    it.textures[i] = gl.createTexture();
-    gl.bindTexture( gl.TEXTURE_2D, it.textures[i] );
-  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
-  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR );
-  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
-  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
-    gl.bindTexture( gl.TEXTURE_2D, null );
-  }
 
 };
 
@@ -87,7 +61,16 @@ ItaPoly.prototype.createProgram = function( _frag, _callback ){
 	if( gl.getProgramParameter( p, gl.LINK_STATUS ) ){
     it.error = '';
 		it.program = p;
-    if( typeof _callback == 'function' ){ _callback( it ); }
+
+    it.vbo = gl.createBuffer();
+		gl.bindBuffer( gl.ARRAY_BUFFER, it.vbo );
+		gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [-1,1,-1,-1,1,1,1,-1] ), gl.STATIC_DRAW );
+
+    var locationP = gl.getAttribLocation( it.program, 'p' );
+  	gl.enableVertexAttribArray( locationP );
+  	gl.vertexAttribPointer( locationP, 2, gl.FLOAT, false, 0, 0 );
+
+    if( typeof _callback === 'function' ){ _callback( it ); }
 	}else{
 		it.error = gl.getProgramInfoLog( p );
     console.error( it.error );
@@ -95,12 +78,21 @@ ItaPoly.prototype.createProgram = function( _frag, _callback ){
 
 };
 
+ItaPoly.prototype.setTime = function( _time ){
+
+  var it = this;
+
+  it.manualTime = true;
+  it.time = _time;
+
+}
+
 ItaPoly.prototype.setFloat = function( _name, _param ){
 
 	var it = this;
 
-  if( !it.location[_name] ){ it.location[_name] = it.gl.getUniformLocation( it.program, _name ); }
-  it.gl.uniform1f( it.location[_name], _param );
+  if( !it.location[ _name ] ){ it.location[ _name ] = it.gl.getUniformLocation( it.program, _name ); }
+  it.gl.uniform1f( it.location[ _name ], _param );
 
 };
 
@@ -108,8 +100,8 @@ ItaPoly.prototype.setVec2 = function( _name, _param ){
 
 	var it = this;
 
-  if( !it.location[_name] ){ it.location[_name] = it.gl.getUniformLocation( it.program, _name ); }
-  it.gl.uniform2fv( it.location[_name], _param );
+  if( !it.location[ _name ] ){ it.location[ _name ] = it.gl.getUniformLocation( it.program, _name ); }
+  it.gl.uniform2fv( it.location[ _name ], _param );
 
 };
 
@@ -117,8 +109,8 @@ ItaPoly.prototype.setVec3 = function( _name, _param ){
 
 	var it = this;
 
-  if( !it.location[_name] ){ it.location[_name] = it.gl.getUniformLocation( it.program, _name ); }
-  it.gl.uniform3fv( it.location[_name], _param );
+  if( !it.location[ _name ] ){ it.location[ _name ] = it.gl.getUniformLocation( it.program, _name ); }
+  it.gl.uniform3fv( it.location[ _name ], _param );
 
 };
 
@@ -126,8 +118,8 @@ ItaPoly.prototype.setVec4 = function( _name, _param ){
 
 	var it = this;
 
-  if( !it.location[_name] ){ it.location[_name] = it.gl.getUniformLocation( it.program, _name ); }
-  it.gl.uniform4fv( it.location[_name], _param );
+  if( !it.location[ _name ] ){ it.location[ _name ] = it.gl.getUniformLocation( it.program, _name ); }
+  it.gl.uniform4fv( it.location[ _name ], _param );
 
 };
 
@@ -135,60 +127,58 @@ ItaPoly.prototype.setInt = function( _name, _param ){
 
 	var it = this;
 
-  if( !it.location[_name] ){ it.location[_name] = it.gl.getUniformLocation( it.program, _name ); }
-  it.gl.uniform1i( it.location[_name], _param );
+  if( !it.location[ _name ] ){ it.location[ _name ] = it.gl.getUniformLocation( it.program, _name ); }
+  it.gl.uniform1i( it.location[ _name ], _param );
 
 };
+
+ItaPoly.prototype.prepareTexture = function( _name ){
+
+  var it = this;
+  var gl = it.gl;
+
+  if( it.textureNumbers[ _name ] !== 0 && !it.textureNumbers[ _name ] ){
+
+    var i = it.textureCount;
+
+    gl.activeTexture( gl.TEXTURE0 + i );
+    it.textures[ i ] = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, it.textures[ i ] );
+  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
+  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR );
+  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
+  	gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
+
+    it.textureNumbers[ _name ] = i;
+    it.location[ i ] = gl.getUniformLocation( it.program, _name );
+
+    it.textureCount += 1;
+
+  }
+
+}
 
 ItaPoly.prototype.setSampler2D = function( _name, _param ){
 
   var it = this;
 	var gl = it.gl;
 
-  if( 4 < it.textureCount ){
+  it.prepareTexture( _name );
 
-    console.error( 'setSampler2D : You cannot use more than 5 textures' );
-
-  }else{
-
-    if( !it.location[_name] ){ it.location[_name] = gl.getUniformLocation( it.program, _name ); }
-    if( !it.textureNumbers[_name] ){
-      it.textureNumbers[_name] = it.textureCount;
-      it.textureCount += 1;
-    }
-
-  	gl.activeTexture( 33984 + it.textureNumbers[_name] ); // gl.TEXTURE0 = 33984
-  	gl.bindTexture( gl.TEXTURE_2D, it.textures[ it.textureNumbers[_name] ] );
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, _param );
-    gl.uniform1i( it.location[_name], it.textureNumbers[_name] );
-
-  }
+	gl.bindTexture( gl.TEXTURE_2D, it.textures[ it.textureNumbers[ _name ] ] );
+  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, _param );
 
 };
 
-ItaPoly.prototype.setSampler2DFromArray = function( _name, _array ){
+ItaPoly.prototype.setSampler2DFromArray = function( _name, _array, _width, _height ){
 
   var it = this;
 	var gl = it.gl;
 
-  if( 4 < it.textureCount ){
+  it.prepareTexture( _name );
 
-    console.error( 'setSampler2D : You cannot use more than 5 textures' );
-
-  }else{
-
-    if( !it.location[_name] ){ it.location[_name] = gl.getUniformLocation( it.program, _name ); }
-    if( !it.textureNumbers[_name] ){
-      it.textureNumbers[_name] = it.textureCount;
-      it.textureCount += 1;
-    }
-
-  	gl.activeTexture( 33984 + it.textureNumbers[_name] ); // gl.TEXTURE0 = 33984
-  	gl.bindTexture( gl.TEXTURE_2D, it.textures[ it.textureNumbers[_name] ] );
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, _array.length / 4, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array( _array ) );
-    gl.uniform1i( it.location[_name], it.textureNumbers[_name] );
-
-  }
+	gl.bindTexture( gl.TEXTURE_2D, it.textures[ it.textureNumbers[ _name ] ] );
+  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, _width, _height, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array( _array ) );
 
 };
 
@@ -201,7 +191,7 @@ ItaPoly.prototype.mousedown = function( _e ){
   it.mouse[2] = _e.offsetX;
   it.mouse[3] = _e.offsetY;
 
-  if( typeof it.onMouseDown == 'function' ){ it.onMouseDown( _e ); }
+  if( typeof it.onMouseDown === 'function' ){ it.onMouseDown( _e ); }
 
 };
 
@@ -214,7 +204,7 @@ ItaPoly.prototype.mouseup = function( _e ){
   it.mouse[2] = - _e.offsetX;
   it.mouse[3] = - _e.offsetY;
 
-  if( typeof it.onMouseUp == 'function' ){ it.onMouseUp( _e ); }
+  if( typeof it.onMouseUp === 'function' ){ it.onMouseUp( _e ); }
 
 };
 
@@ -224,8 +214,12 @@ ItaPoly.prototype.mousemove = function( _e ){
 
   it.mouse[0] = _e.offsetX;
   it.mouse[1] = _e.offsetY;
+  if( 0 < it.mouse[2] ){
+    it.mouse[2] = _e.offsetX;
+    it.mouse[3] = _e.offsetY;
+  }
 
-  if( typeof it.onMouseMove == 'function' ){ it.onMouseMove( _e ); }
+  if( typeof it.onMouseMove === 'function' ){ it.onMouseMove( _e ); }
 
 };
 
@@ -234,17 +228,21 @@ ItaPoly.prototype.update = function(){
 	var it = this;
 	var gl = it.gl;
 
-  it.time = ( it.beginTime - ( +new Date() ) ) / 1000.0;
-
 	gl.clearColor( 0, 0, 0, 1 );
   gl.clearDepth( 1 );
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
 	gl.useProgram( it.program );
 
+  it.time = ( ( +new Date() ) - it.beginTime ) / 1000.0;
 	it.setFloat( 'time', it.time );
 	it.setVec2( 'resolution', it.resolution );
   it.setVec4( 'mouse', it.mouse );
+
+  for( var i=0; i<it.textures.length; i++ ){
+    gl.bindTexture( gl.TEXTURE_2D, it.textures[ i ] );
+    gl.uniform1i( it.location[ i ], i );
+  }
 
 	gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
 
